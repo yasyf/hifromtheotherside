c7fef3780ca486ba9a51994c9019794448926328
Jesse Dunietz <yavyash@gmail.com>
2016-12-10 20:10:46 -0500

    Added search for ZIP and supported candidate

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -15,7 +15,22 @@ class AdminController < ApplicationController
       scope = scope.unpaired
       @title = 'Unpaired Users'
     end
-    scope = scope.fuzzy_search(params[:q]) if params[:q].present?
+
+    name_email = params[:name_email]
+    zip = params[:zip]
+    supported = params[:supported]
+
+    if name_email.present?
+         scope = scope.fuzzy_search(name_email) # Searches all string fields; presumably won't match ZIP
+    end
+    if zip.present?
+      # The first 3 digits of a ZIP code indicate a postal delivery region.
+      scope = scope.zip_starts_with(zip[0..2])
+    end
+    if supported.present?
+      scope = scope.where(supported: supported.to_i)
+    end
+
     @pages = scope.count('*') / USER_LIMIT
     @users = scope.order(:created_at).limit(USER_LIMIT).offset(page * USER_LIMIT)
   end
