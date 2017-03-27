8262b7a7b8a9e1205f375bd7801a27f9272f1990
Yasyf Mohamedali <yasyfm@gmail.com>
2017-03-27 17:01:19 +0900

    Handle blank emails. Closes #53.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,15 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in @user, :event => :authentication
-    set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    begin
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+    rescue ActiveRecord::RecordInvalid
+      flash.alert = "Your email is required to continue!"
+      redirect_to root_path
+    else
+      sign_in @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    end
+
     redirect_to new_response_path
   end
 
